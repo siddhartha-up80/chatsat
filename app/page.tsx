2467a1cb61e517d0cd9ae36906647c0c5c06d93e
Siddhartha Singh <84330754+siddhartha-up80@users.noreply.github.com>
2024-02-18 18:57:36 +0530

    done with changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,112 +1,252 @@
-import Image from "next/image";
+"use client";
+
+import { LogOut, Medal, TrendingUp } from "lucide-react";
+import {
+  CircularProgressbarWithChildren,
+  buildStyles,
+} from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+
+const value = 0.77;
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+      <div className="flex min-h-screen">
+        <div className="w-1/3 bg-gradient-to-b from-[#374841] to-[#2D2D2D] shadow-xl">
+          <section className="px-5 py-3 flex flex-col gap-3">
+            <div className="flex items-center">
+              <span className="rounded-full bg-white w-4 h-4 inline-block mr-2"></span>
+              <span className="text-white text-lg">Chat</span>
+              <span className="text-white font-semibold text-lg">SAT</span>
+            </div>
+            <div className="shadow-md">
+              <button className="text-white bg-opacity-10 bg-white py-3 px-4 rounded-xl w-full text-start">
+                New Thread
+              </button>
+            </div>
+
+            <div>
+              <div className="flex justify-between gap-2 bg-opacity-10 bg-white py-3 px-4 rounded-xl w-full items-center shadow-md">
+                <div>
+                  <div
+                    style={{ width: "100px", height: "100px", display: "flex" }}
+                  >
+                    <CircularProgressbarWithChildren
+                      value={value}
+                      maxValue={1}
+                      styles={buildStyles({
+                        pathColor: `rgba(163, 230, 53, ${value})`,
+                        textColor: "#fff",
+                        trailColor: "#374841",
+                        backgroundColor: "#3e98c7",
+                      })}
+                    >
+                      <div className="flex flex-col items-center leading-tight">
+                        <span className="text-lime-400 font-semibold">4/7</span>
+                        <span className="font-semibold text-white text-sm">
+                          Streak
+                        </span>
+                      </div>
+                    </CircularProgressbarWithChildren>
+                  </div>
+                </div>
+                <div className="flex flex-col gap-2">
+                  <span className="flex gap-1">
+                    <Medal className="text-lime-400" />{" "}
+                    <span className="text-white">
+                      0/233 <span className="text-lime-400 font-bold">XP</span>
+                    </span>
+                  </span>
+                  <span>
+                    <button className=" bg-opacity-10 bg-white py-2 px-4 rounded-md  text-lime-400">
+                      Take a Quiz
+                    </button>
+                  </span>
+                </div>
+              </div>
+            </div>
+
+            <div className="bg-opacity-10 bg-white rounded-xl flex flex-col gap-2">
+              <div>
+                <button className="text-white bg-opacity-15 bg-white py-4 px-4 rounded-xl w-full text-start">
+                  Leaderboard
+                </button>
+              </div>
+              <div className="px-3 pb-4 gap-2 flex flex-col">
+                <div className="text-white bg-opacity-15 bg-white py-3 px-4 rounded-xl w-full flex justify-between">
+                  <span className="flex items-center gap-1">
+                    01. <Medal />{" "}
+                    <span className="rounded-full bg-white w-4 h-4 inline-block"></span>
+                    Name
+                  </span>
+
+                  <span>
+                    0/233 <span className="text-lime-400 font-bold">XP</span>
+                  </span>
+                </div>
+                <div className="text-white bg-opacity-15 bg-white py-3 px-4 rounded-xl w-full flex justify-between">
+                  <span className="flex items-center gap-1">
+                    02. <Medal />{" "}
+                    <span className="rounded-full bg-white w-4 h-4 inline-block"></span>
+                    Name
+                  </span>
+
+                  <span>
+                    0/233 <span className="text-lime-400 font-bold">XP</span>
+                  </span>
+                </div>
+                <div className="text-white bg-opacity-15 bg-white py-3 px-4 rounded-xl w-full flex justify-between">
+                  <span className="flex items-center gap-1">
+                    03. <Medal />{" "}
+                    <span className="rounded-full bg-white w-4 h-4 inline-block"></span>
+                    Name
+                  </span>
+
+                  <span>
+                    0/233 <span className="text-lime-400 font-bold">XP</span>
+                  </span>
+                </div>
+                <div className="text-white bg-opacity-15 bg-gradient-to-b from-lime-400/50 to-sky-900/30 py-3 px-4 rounded-xl w-full flex justify-between">
+                  <span className="flex items-center gap-1">
+                    54. <Medal />{" "}
+                    <span className="rounded-full bg-white w-4 h-4 inline-block"></span>
+                    Name
+                  </span>
+
+                  <span>
+                    0/233 <span className="text-lime-400 font-bold">XP</span>
+                  </span>
+                </div>
+              </div>
+            </div>
+
+            <div className="flex flex-col gap-2">
+              <div className="flex gap-2 items-center">
+                <span className="rounded-full bg-white w-8 h-8 inline-block"></span>
+                <span className="text-white">Name</span>
+              </div>
+              <div className="flex gap-1 items-center">
+                <span>
+                  <LogOut className="text-white text-sm w-4" />
+                </span>
+                <span className="text-white  text-sm">Sign Out</span>
+              </div>
+            </div>
+          </section>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <div className="bg-gradient-to-br from-[#2D2D2D] to-[#2E3732] w-full">
+          <section className="px-20 py-10">
+            <div className="flex flex-col gap-6 ">
+              <div className="flex flex-col leading-tight">
+                <div>
+                  <span className="text-white font-extrabold text-3xl">
+                    Hey,{" "}
+                  </span>
+                  <span className="font-extrabold text-3xl text-lime-400">
+                    Name!
+                  </span>
+                </div>
+                <div>
+                  <span className="text-white font-extrabold text-3xl">
+                    How's your prep for the{" "}
+                  </span>
+                  <span className="font-extrabold text-3xl text-lime-400">
+                    March SAT
+                  </span>
+                </div>
+              </div>
+              <div>
+                <div className="grid-cols-2 grid gap-4 w-max">
+                  <div className="box flex flex-col bg-[#404040] max-w-[20rem] min-h-[15rem] p-4 rounded-2xl">
+                    <div className="flex flex-col leading-tight text-sm text-gray-500">
+                      <span>Last Practice Linear etc etc</span>
+                      <span>28/02/23</span>
+                    </div>
+                    <div>
+                      <div>
+                        <TrendingUp className="text-white w-24 h-24" />
+                      </div>
+                    </div>
+                    <div>
+                      <span className="text-3xl text-white font-semibold">
+                        Practice
+                      </span>
+                    </div>
+                    <div className="leading-none">
+                      <span className="text-sm text-white">
+                        A description of the above parameter goes here.
+                      </span>
+                    </div>
+                  </div>{" "}
+                  <div className="box flex flex-col bg-[#404040] max-w-[20rem] min-h-[15rem] p-4 rounded-2xl">
+                    <div className="flex flex-col leading-tight text-sm text-gray-500">
+                      <span>Last Practice Linear etc etc</span>
+                      <span>28/02/23</span>
+                    </div>
+                    <div>
+                      <div>
+                        <TrendingUp className="text-white w-24 h-24" />
+                      </div>
+                    </div>
+                    <div>
+                      <span className="text-3xl text-white font-semibold">
+                        Mock
+                      </span>
+                    </div>
+                    <div className="leading-none">
+                      <span className="text-sm text-white">
+                        A description of the above parameter goes here.
+                      </span>
+                    </div>
+                  </div>{" "}
+                  <div className="box flex flex-col bg-[#404040] max-w-[20rem] min-h-[15rem] p-4 rounded-2xl">
+                    <div className="flex flex-col leading-tight text-sm text-gray-500">
+                      <span>Last Practice Linear etc etc</span>
+                      <span>28/02/23</span>
+                    </div>
+                    <div>
+                      <div>
+                        <TrendingUp className="text-white w-24 h-24" />
+                      </div>
+                    </div>
+                    <div>
+                      <span className="text-3xl text-white font-semibold">
+                        Title Text Here
+                      </span>
+                    </div>
+                    <div className="leading-none">
+                      <span className="text-sm text-white">
+                        A description of the above parameter goes here.
+                      </span>
+                    </div>
+                  </div>{" "}
+                  <div className="box flex flex-col bg-[#404040] max-w-[20rem] min-h-[15rem] p-4 rounded-2xl">
+                    <div className="flex flex-col leading-tight text-sm text-gray-500">
+                      <span>Last Practice Linear etc etc</span>
+                      <span>28/02/23</span>
+                    </div>
+                    <div>
+                      <div>
+                        <TrendingUp className="text-white w-24 h-24" />
+                      </div>
+                    </div>
+                    <div>
+                      <span className="text-3xl text-white font-semibold">
+                        Title Text Here
+                      </span>
+                    </div>
+                    <div className="leading-none">
+                      <span className="text-sm text-white">
+                        A description of the above parameter goes here.
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+        </div>
       </div>
     </main>
   );
